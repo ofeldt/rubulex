bf552b7ddd6a4dea0532f198d41fccfd65bb3708
Oliver Feldt <oliver.feldt@googlemail.com>
2013-06-17 23:01:28 +0200

    fixed regex parser not matching groupless results properly

--- lib/rubulex/regexp_parser.rb
+++ lib/rubulex/regexp_parser.rb
@@ -35,8 +35,8 @@ module Rubulex
 
     def parse
       @data.match(@regex) do |match|
-        @match_result = render_match_result(@data)
-        @match_groups = render_match_groups(@data)
+        @match_result = render_match_result(@data.dup)
+        @match_groups = render_match_groups(@data.dup)
       end
     end
 
@@ -44,12 +44,11 @@ module Rubulex
       colors = ->() do
         (@colors ||= [:red, :green, :darkorange, :blue].cycle).next
       end
-      match_result = @data.dup
-      match_result.gsub!(@regex) do |match|
+      data.gsub!(@regex) do |match|
         "<span class='#{colors.call}'>#{match}</span>"
       end
 
-      match_result.gsub!(/\n/,"<br />")
+      data.gsub(/\n/,"<br />")
     end
 
     def render_match_groups(data)
@@ -58,16 +57,19 @@ module Rubulex
 
       data.gsub(@regex) do |match_text|
         sub_match = match_text.match(@regex)
-        sub_match_set = []
+        match_group_count = sub_match.length - 1
+        if match_group_count > 0
+          sub_match_set = []
 
-        (sub_match.length - 1).times do |index|
-          key = sub_match.names[index] || index + 1
-          match_text = sub_match[key]
+          match_group_count.times do |index|
+            key = sub_match.names[index] || index + 1
+            match_text = sub_match[key]
 
-          sub_match_set << Struct::MatchRelation.new(key, match_text)
+            sub_match_set << Struct::MatchRelation.new(key, match_text)
+          end
+
+          match_groups << sub_match_set
         end
-
-        match_groups << sub_match_set
       end
 
       match_groups.map.with_index { |sub_set, index| 
